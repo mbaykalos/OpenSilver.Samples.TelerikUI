c53e9f44afddeb16c3d08ea49220e324a05508c1
cshtml5 <contact@cshtml5.com>
2020-09-09 00:45:47 +0200

    Update OpenSilver NuGet package reference from version "1.0.0-alpha-006" to "1.0.0-alpha-008".

--- OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI.Browser/Program.cs
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI.Browser/Program.cs
@@ -1,25 +1,33 @@
-﻿using DotNetForHtml5;
-using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using DotNetForHtml5;
 
 namespace OpenSilver.Samples.TelerikUI.Browser
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             Cshtml5Initializer.Initialize();
-            IWebAssemblyHost host = CreateHostBuilder(args).Build();
-            host.Run();
-            host.Dispose();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            var host = builder.Build();
+            await host.RunAsync();
+        }
+
+        public static void RunApplication()
+        {
             Application.RunApplication(() =>
             {
                 var app = new OpenSilver.Samples.TelerikUI.App();
             });
         }
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
     }
 }
