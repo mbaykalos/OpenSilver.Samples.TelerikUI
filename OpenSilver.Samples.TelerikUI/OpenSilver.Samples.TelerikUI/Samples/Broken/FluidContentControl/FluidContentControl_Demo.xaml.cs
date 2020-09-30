0372106961752aa046a4961ca4e9cfb9c91eb965
Sylvain Maillard <maillard.sy@gmail.com>
2020-09-30 16:10:55 +0200

    Add FluidContentControl + update RadTransitionControl demo with a compatibility version.

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/Broken/FluidContentControl/FluidContentControl_Demo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace OpenSilver.Samples.TelerikUI
+{
+    public partial class FluidContentControl_Demo : UserControl
+    {
+        public FluidContentControl_Demo()
+        {
+            this.InitializeComponent();
+        }
+
+        bool isSmall = true;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(isSmall)
+            {
+                FluidContentControlContainer.Width = 500;
+                FluidContentControlContainer.Height = 500;
+                isSmall = false;
+            }
+            else
+            {
+                FluidContentControlContainer.Width = 70;
+                FluidContentControlContainer.Height = 70;
+                isSmall = false;
+            }
+        }
+    }
+}
--- OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/InProgress/RadTransitionControl/RadTransitionControl_Demo.xaml.cs
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/InProgress/RadTransitionControl/RadTransitionControl_Demo.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Navigation;
+using Telerik.Windows.Controls;
 
 namespace OpenSilver.Samples.TelerikUI
 {
@@ -34,6 +35,23 @@ namespace OpenSilver.Samples.TelerikUI
                      FilePathOnGitHub = "github.com/OpenSilver/OpenSilver.Samples.TelerikUI/blob/master/OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI/Samples/InProgress/RadTransitionControl/RadTransitionControl_Demo.xaml.cs"
                 }
             });
+        }
+
+        bool isButtonInVisualTree = true;
+        RadButton myRadButton;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isButtonInVisualTree)
+            {
+                myRadButton = MyRadButton;
+                radTransitionControl.Content = null;
+                isButtonInVisualTree = false;
+            }
+            else
+            {
+                radTransitionControl.Content = myRadButton;
+                isButtonInVisualTree = true;
+            }
         }
     }
 }
