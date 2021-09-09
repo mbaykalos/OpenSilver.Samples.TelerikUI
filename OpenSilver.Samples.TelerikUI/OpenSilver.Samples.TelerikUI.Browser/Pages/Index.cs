84a647e2b2cbc2bf7bf484196fe56f6753afdf7f
Iakov Lilo <jlilo@hwdtech.com>
2021-09-09 17:53:05 +0600

    Migrate to OpenSilver Beta

---
+++ OpenSilver.Samples.TelerikUI/OpenSilver.Samples.TelerikUI.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using OpenSilver.Samples.TelerikUI.Browser.Interop;
+
+namespace OpenSilver.Samples.TelerikUI.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
